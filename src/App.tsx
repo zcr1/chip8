9c13ef60dc17d3becbafc98523d233006ae22792
Zach Reinhardt <zach@novacredit.com>
2025-08-10 14:05:58 -0500

    Add sass

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import './App.css';
+import './App.scss';
 
 import { useAppStore } from './store';
 
--- src/env.d.ts
+++ src/env.d.ts
@@ -5,7 +5,7 @@
  * @requires [@rsbuild/plugin-svgr](https://npmjs.com/package/@rsbuild/plugin-svgr)
  */
 declare module '*.svg?react' {
-  import type React from 'react';
-  const ReactComponent: React.FunctionComponent<React.SVGProps<SVGSVGElement>>;
-  export default ReactComponent;
+	import type React from 'react';
+	const ReactComponent: React.FunctionComponent<React.SVGProps<SVGSVGElement>>;
+	export default ReactComponent;
 }
