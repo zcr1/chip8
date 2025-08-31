729c14d89b6071bdb13a1f80db59b6987c69ed07
Zach Reinhardt <zach@novacredit.com>
2025-08-31 13:42:47 -0500

    Add normalize

--- rsbuild.config.ts
+++ rsbuild.config.ts
@@ -6,5 +6,15 @@ export default defineConfig({
 	plugins: [pluginSass(), pluginReact()],
 	html: {
 		title: 'Chip8 Emulator',
+		tags: [
+			{
+				tag: 'link',
+				attrs: {
+					rel: 'stylesheet',
+					href: 'https://cdn.jsdelivr.net/npm/normalize.css@8.0.1/normalize.css',
+				},
+				head: true,
+			},
+		],
 	},
 });
